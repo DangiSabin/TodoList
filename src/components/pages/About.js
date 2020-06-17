63b1c623c6691ba1d4cebb4c59512a3f1581f874
sabin dangi <sabin4.dangi@gmail.com>
2020-06-17 00:26:50 -0600

    Initial code commit for TodoList React JS app

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>About</h1>
+                <p>This is the TodoList version v0.0.1</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default About
